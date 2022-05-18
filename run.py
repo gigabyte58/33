957cc2464e1c7738e906196116ed56f67dd9ff49
gigabyte58 <104204078+gigabyte58@users.noreply.github.com>
2022-05-18 20:16:54 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,2 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-    os.system('pip install bs4')
-    os.system('pip install futures')
- 
-import requests
-bit = platform.architecture()[0]
-if bit == '32bit':
-    __import__('ub').check()
- 
+from brutec import buy
+buy()
